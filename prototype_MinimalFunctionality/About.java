9fbfcdf955860f84694ce29ca221b5076a374640
alexluongo <32210000+alexluongo@users.noreply.github.com>
2017-11-24 20:18:12 -0500

    About.java appearance

--- prototype_MinimalFunctionality/About.java
+++ prototype_MinimalFunctionality/About.java
@@ -10,13 +10,15 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import java.awt.Font;
+import java.awt.Color;
 
 public class About extends JDialog implements ActionListener {
 	
 	private String credits = "This application uses the Open Source components below, listed with their copyright and links to their source and license information.\n\n"
 			+ "c3p0\n"
 			+ "Project code: https://github.com/swaldman/c3p0\n"
-			+ "by Steve Waldman <swaldman@mchange.com> © 2015 Machinery For Change, Inc.\n"
+			+ "by Steve Waldman <swaldman@mchange.com> Â© 2015 Machinery For Change, Inc.\n"
 			+ "License: (LGPL) https://github.com/swaldman/c3p0/blob/master/LICENSE-LGPL\n\n"
 			+ "jBCrypt\n"
 			+ "Project code: https://github.com/jeremyh/jBCrypt\n"
@@ -28,7 +30,7 @@ public class About extends JDialog implements ActionListener {
 			+ "License: (MIT) https://github.com/LGoodDatePicker/LGoodDatePicker/blob/master/LICENSE\n\n"
 			+ "WebLaF\n"
 			+ "Project code: https://github.com/mgarin/weblaf\n"
-			+ "© WebLaF - Java Look and Feel 2017\n"
+			+ "Â© WebLaF - Java Look and Feel 2017\n"
 			+ "License: (GPLv3) https://www.gnu.org/licenses/gpl-3.0.html\n\n"
 			+ "mysqlconnector\n"
 			+ "Project code: https://github.com/swaldman/c3p0\n"
@@ -40,6 +42,8 @@ public class About extends JDialog implements ActionListener {
 		JPanel messagePane = new JPanel();
 	    
 	    JTextPane textPane = new JTextPane();
+	    textPane.setForeground(new Color(0, 0, 51));
+	    textPane.setFont(new Font("Tahoma", Font.BOLD, 13));
 	    messagePane.add(textPane);
 	    textPane.setText(credits);
 	    textPane.setEditable(false);
