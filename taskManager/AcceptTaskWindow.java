474eb17b315aa0710bd074e93b99864a7323eb8d
Matthew C <mczechmczech@gmail.com>
2017-11-29 17:31:14 -0500

    Update AcceptTaskWindow.java

--- taskManager/AcceptTaskWindow.java
+++ taskManager/AcceptTaskWindow.java
@@ -42,7 +42,7 @@ public class AcceptTaskWindow
 	private Integer[] priority = {1, 2, 3, 4, 5};
 	private final JComboBox<String> cbPercentComplete = new JComboBox(completion);
 	private final JComboBox<Integer> cbPriority = new JComboBox(priority);
-	
+	private static Image iconImg; 
 	//this constructor is for editing tasks
 	public AcceptTaskWindow(Task task, MainWindow pWindow) {
 		EventQueue.invokeLater(new Runnable() {
@@ -62,6 +62,8 @@ public class AcceptTaskWindow
 	//initialize method for any new EditTaskWindow object
 	private void initialize(Task t, MainWindow pWin)
 	{
+		iconImg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/taskManager/Infinity_1.png"));
+		frmAcceptTaskWindow.setIconImage(iconImg);
 		frmAcceptTaskWindow = new JFrame();
 		frmAcceptTaskWindow.setTitle("Accept/Decline");
 		frmAcceptTaskWindow.setBounds(100, 100, 450, 325);
