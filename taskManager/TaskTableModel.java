a3d7f6119c5b218199392b2d0728b8965b98c869
Joshua <golding.joshua8@gmail.com>
2017-12-20 19:44:43 -0500

    Javadoc for TaskTableModel

--- taskManager/TaskTableModel.java
+++ taskManager/TaskTableModel.java
@@ -2,11 +2,19 @@ package taskManager;
 
 import javax.swing.table.DefaultTableModel;
 
+/**
+ * The MessageTableModel class is an extension of DefaultTableModel
+ * which uses a Vector of Vectors to store cell value objects.
+ *
+ *@version 12.20.2017
+ */
 public class TaskTableModel extends DefaultTableModel {
 
 	/**
-	 * @param columnNames
-	 * @param i
+	 * Construct this task table model
+	 * 
+	 * @param columnNames the column names of this task table model
+	 * @param i the number of rows in this task table model
 	 */
 	public TaskTableModel(String[] columnNames, int i) {
 		super(columnNames, i);
